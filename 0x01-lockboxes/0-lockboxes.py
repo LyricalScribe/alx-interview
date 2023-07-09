59f86d74b1af88da4c9cd0c605b5a090d85e3d34
Thulisile Ngomane <ThuleeN@outlook.com>
2023-07-10 00:23:44 +0200

    Added locked boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""Method to determine lockedboxes"""
+
+
+def canUnlockAll(boxes):
+    unlocked = [0]
+
+    for box_id in range(len(boxes)):
+        box = boxes[box_id]
+
+        if len(box) == 0:
+            continue
+        for key in box:
+            if key not in unlocked:
+                unlocked.append(key)
+
+    return len(unlocked) == len(boxes)
