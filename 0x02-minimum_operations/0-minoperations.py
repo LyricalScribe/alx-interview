fab40bde89453e2ec8be31d0faa5566ddf9dbc1f
Thulisile Ngomane <ThuleeN@outlook.com>
2023-07-23 23:59:43 +0200

    fixed comment error

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """method that calculates the fewest number 
 of operations needed to result in exactly 
-n H characters in the file""""
+n H characters in the file"""
 
 def minOperations(n):
     """minimum operations"""
