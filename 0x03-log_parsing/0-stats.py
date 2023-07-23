4bfc21c891889ae0cddb01c09b703de76bec65e3
Thulisile Ngomane <ThuleeN@outlook.com>
2023-07-23 23:42:01 +0200

    fixed pycodestyle

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,49 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics """
+"""Script that reads stdin line by line and computes metrics."""
 
-if __name__ == '__main__':
+import sys
 
-    import sys
+Codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-    Codes = {"200": 0,
-                   "301": 0,
-                   "400": 0,
-                   "401": 0,
-                   "403": 0,
-                   "404": 0,
-                   "405": 0,
-                   "500": 0
-                   }
-    fileSize = 0
-    counter = 0
+fileSize = 0
+counter = 0
 
 
-    def print_results(Codes, fileSize):
-        """Print values"""
-        print("File size: {:d}".format(fileSize))
-        for Code, times in sorted (Codes.items()):
-            if times:
-                print("{:s}: {:d}".format(Code, times))
-    try:
-        """Read stdin per line"""
-        for line in sys.stdin:
-            if counter != 0 and counter == 10:
-                """print from the beginning after every 10 lines"""
-                print_results(Codes, fileSize)
-            counter += 1
-            data = line.split()
-            try:
-                """Calculate metrics"""
-                Code = data[-2]
-                if Code in Codes:
-                    Codes[Code] += 1
-                fileSize += int(data[-1])
-            except:
-                pass
-        print_results(Codes, fileSize)
-    except KeyboardInterrupt:
-        """Keyboard interruption, print from the beginning"""
-        print_results(Codes, fileSize)
-        raise
+def print_results(Codes, fileSize):
+    """Print values."""
+    print("File size: {:d}".format(fileSize))
+    for Code, times in sorted(Codes.items()):
+        if times:
+            print("{:s}: {:d}".format(Code, times))
+
+
+try:
+    """Read stdin per line."""
+    for line in sys.stdin:
+        if counter != 0 and counter == 10:
+            """Print from the beginning after every 10 lines."""
+            print_results(Codes, fileSize)
+        counter += 1
+        data = line.split()
+        try:
+            """Calculate metrics."""
+            Code = data[-2]
+            if Code in Codes:
+                Codes[Code] += 1
+            fileSize += int(data[-1])
+        except Exception:
+            pass
+    print_results(Codes, fileSize)
+except KeyboardInterrupt:
+    """Keyboard interruption, print from the beginning."""
+    print_results(Codes, fileSize)
+    raise
