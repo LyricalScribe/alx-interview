7734bf48c808731332a32525dc81b708a0360999
Thulisile Ngomane <ThuleeN@outlook.com>
2023-08-31 23:45:44 +0200

    Calculated perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Calculate island perimeter"""
+
+def island_perimeter(grid):
+    """Calculate perimeter"""
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] != 0:
+
+                if j != 0:
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+
+                if j < cols - 1:
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+
+                if i != 0:
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
+
+                if i < rows - 1:
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+
+    return perimeter
