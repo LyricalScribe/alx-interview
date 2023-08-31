e9c6315598ea3ef0920f67d0be97e40f15484fd7
Thulisile Ngomane <ThuleeN@outlook.com>
2023-09-01 00:20:19 +0200

    Added comments

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,19 +11,19 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] != 0:
-
+                # Check left
                 if j != 0:
                     if grid[i][j - 1] == 0:
                         perimeter += 1
-
+                # Check right
                 if j < cols - 1:
                     if grid[i][j + 1] == 0:
                         perimeter += 1
-
+                # Check up
                 if i != 0:
                     if grid[i - 1][j] == 0:
                         perimeter += 1
-
+                # Check down
                 if i < rows - 1:
                     if grid[i + 1][j] == 0:
                         perimeter += 1
